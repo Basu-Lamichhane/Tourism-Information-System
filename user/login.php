c7878d9942a88be71e13912c15bb9071ef061491
pawanAXiX <pawanregmi86@gmail.com>
2023-04-24 22:21:48 +0545

    login file

---
+++ user/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+    <link rel="stylesheet" href="style/home_style.css">
+    <link rel="stylesheet" href="style/nav_style.css">
+    <link rel="stylesheet" href="style/login_style.css">
+</head>
+<body>
+    <?php include "include/nav.inc.php"; ?>
+
+    <div class="register_container">
+        <?php include "include/login.inc.php"; ?>
+    </div>
+    
+</body>
+</html>
