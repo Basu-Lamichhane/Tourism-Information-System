5856d79d4d8e4f87fa0c3b35e02ecf266818451c
Basu Lamichhane <basulamichane991@gmail.com>
2023-05-28 20:39:12 +0545

    Admin homepage format created

---
+++ admin/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sidebar Menu for Admin Dashboard | Traverse Nepal</title>
+    <link rel="stylesheet" href="./style/style.css" />
+</head>
+
+<body>
+    <nav class="sidebar">
+        <a href="#" class="logo">
+        <span class="logo-image"></span>    
+        <span class="logo-name">Traverse Nepal</span>
+        </a>
+
+        <div class="menu-content">
+            <ul class="menu-items">
+                <div class="menu-title">Your menu title</div>
+
+                <li class="item">
+                    <a href="#">Your first link</a>
+                </li>
+
+                <li class="item">
+                    <div class="submenu-item">
+                        <span>First submenu</span>
+                        <span class="arrow-right"></span>
+                    </div>
+
+                    <ul class="menu-items submenu">
+                        <div class="menu-title">
+                            <span class="arrow-left"></span>
+                            Your submenu title
+                        </div>
+                        <li class="item">
+                            <a href="#">First sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">First sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">First sublink</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="item">
+                    <div class="submenu-item">
+                        <span>Second submenu</span>
+                        <span class="arrow-right"></span>
+                    </div>
+
+                    <ul class="menu-items submenu">
+                        <div class="menu-title">
+                            <span class="arrow-left"></span>
+                            Your submenu title
+                        </div>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Second sublink</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="item">
+                    <a href="#">Your second link</a>
+                </li>
+
+                <li class="item">
+                    <a href="#">Your third link</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <nav class="navbar">
+        <span class="fa-solid nav-bars" id="sidebar-close"></span>
+    </nav>
+
+    <main class="main">
+        <h1>Admin Dashboard Content</h1>
+    </main>
+
+    <script src="./script/script.js"></script>
+</body>
+
+</html>
