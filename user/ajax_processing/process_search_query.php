cc28b72261320b9ace356b938790885dd546e7a7
Basu Lamichhane <basulamichane991@gmail.com>
2023-06-02 15:57:10 +0545

    updated search page ajax -3

--- user/ajax_processing/process_search_query.php
+++ user/ajax_processing/process_search_query.php
@@ -1,6 +1,13 @@
 <?php
 
-if (isset($_POST["query"])) {
+if (($_SERVER['REQUEST_METHOD'] == 'POST')) {
+
+    $query = $_POST["query"];
+    $destination = $_POST["destination"];
+    $district = $_POST["district"];
+    $type = $_POST["type"];
+
+    // echo $destination;
 
     // require "./include/db_files/dbconn.inc.php";
     $con = new mysqli("localhost", "root", "", "db_tis"); //creating connection
@@ -14,19 +21,37 @@ if (isset($_POST["query"])) {
 
     $condition = preg_replace("/[^A-Za-z\s]/", "", $_POST["query"]);
 
-    $query = "select p_name from tbl_place where p_name LIKE '%" . $condition . "%' order by p_id desc Limit 5 ";
 
+    // if($destination != "")
+    // {
+    //     $query = "select ".$first_char."_name,".$first_char."_image,".$first_char."_address,".$first_char."_district,".$first_char."_type, from tbl_".$destination." where ".$first_char."_name LIKE '%" . $condition . "%' order by ".$first_char."_id desc Limit 5 ";
+    // }
+
+    $first_char = $destination[0];
+    // echo $first_char;
+    $query = "select " . $first_char . "_id" .",". $first_char . "_name" . /**/"," . $first_char . "_address," . $first_char . "_district," . $first_char . "_type" . /**/" from tbl_" . $destination . " where " . $first_char . "_name LIKE '%" . $condition . "%' order by " . $first_char . "_id desc Limit 5 ";
+
+    // echo  $query;
     $result = $con->query($query);
 
-    $replace_string = '<b>' . $condition . '</b>';
+    // echo $result;
+    while ($result_row = $result->fetch_assoc()) {
 
-    foreach ($result as $row) {
-        $data[] = array(
-            'p_name' => str_ireplace($condition, $replace_string, $row["p_name"])
-        );
+        // echo $result_row;
+        $replace_string = '<b>' . $condition . '</b>';
+
+        foreach ($result as $row) {
+            $data[] = array(
+                $first_char . "_name" => str_ireplace($condition, $replace_string, $result_row[$first_char . "_name"]),
+                // $first_char . "_image" => $result_row[$first_char . "_image"],
+                $first_char . "_address" => $result_row[$first_char . "_address"],
+                $first_char . "_district" => $result_row[$first_char . "_district"],
+                $first_char . "_type" => $result_row[$first_char . "_type"]
+            );
+        }
+
     }
 
-    
 
     echo json_encode($data);
 }
--- user/script/ajax_script/search_query_suggestion.js
+++ user/script/ajax_script/search_query_suggestion.js
@@ -1,8 +1,22 @@
 function load_search_result(search_query){
     if(search_query.length > 2)
     {
+
+        var dest_selected=document.getElementById("destination").value;
+        var dist_selected=document.getElementById("district").value;
+        var type_selected=document.getElementById("type").value;
+
+        // console.log(dest_selected,dist_selected,type_selected);
+
+
         var form_data= new FormData();
         form_data.append("query",search_query);
+        form_data.append("destination",dest_selected);
+        form_data.append("district",dist_selected);
+        form_data.append("type",type_selected);
+        // console.log(form_data);
+
+
         var ajax_request =new XMLHttpRequest();
         ajax_request.open("POST","./ajax_processing/process_search_query.php");
         // console.log(form_data);
@@ -14,28 +28,29 @@ function load_search_result(search_query){
                 console.log(ajax_request.responseText);
                 var response =JSON.parse(ajax_request.responseText);
 
+                console.log(response);
                 var html = "<div class='list-results'>";
-
+                html+= "<table class='table-results'>";
+                html+= "<thead><tr><th>S.N</th><th>Destination Name</th><th>address</th><th>district</th><th>type</th></tr></thead><tbody>";
                 if(response.length > 0)
                     {
                         for (let count = 0; count < response.length; count++) {
-                            html += "<a href=# class='list-results' disabled>"+response[count].p_name+"</a>";
-                            
+                            html += "<tr><td>"+(count+1)+"</td><td><a href=# class='list-result' disabled>"+response[count].p_name+"</a></td><td>"+response[count].p_address+"</td><td>"+response[count].p_district+"</td><td>"+response[count].p_type+"</td></tr>";
                         }
                     }
                     else
                     {
-                        html += "<a href=# class='list-results' disabled> No data found </a>";
+                        html += "<a href=# class='list-result' disabled> No data found </a>";
                     }
-                html+="</div>";
+                html+="</tbody></table></div><br>"; 
 
-                document.getElementById("search_result").innerHTML=html;
+                document.getElementById("search_results").innerHTML=html;
             }   
         }
     }
     else
     {
-        document.getElementById("search_result").innerHTML="";
+        document.getElementById("search_results").innerHTML="";
     }
 }
 
