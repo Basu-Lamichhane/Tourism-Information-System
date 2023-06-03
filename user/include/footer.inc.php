c15fed07ff041d7d5e626d5505c16ab74c6ce79d
Basu Lamichhane <basulamichane991@gmail.com>
2023-06-03 14:23:23 +0545

    footer added

---
+++ user/include/footer.inc.php
@@ -0,0 +1,53 @@
+    <div class="footer-section">
+
+        <div class="footer-left">
+            <h3>Traverse<span> Nepal</span></h3>
+
+            <p class="footer-links">
+                <a href="#">Home</a>
+                |
+                <a href="#">About</a>
+                |
+                <a href="#">Contact</a>
+                |
+                <a href="#">Support</a>
+            </p>
+
+            <p class="footer-company-name">Copyright © 2021 <strong>traversenepal</strong> All rights reserved</p>
+        </div>
+
+        <div class="footer-center">
+            <div>
+                <i class="fa fa-map-marker"></i>
+                <p><span>Bharatpur</span>
+                    Chitwan</p>
+            </div>
+
+            <div>
+                <i class="fa fa-phone"></i>
+                <p>+977 9800000000</p>
+            </div>
+            <div>
+                <i class="fa fa-envelope"></i>
+                <p><a href="mailto:traversenepal@gmail.com">traversenepal@gmail.com</a></p>
+            </div>
+        </div>
+        <div class="footer-right">
+            <p class="footer-company-about">
+                <span>About the company</span>
+                <strong>Traverse Nepal</strong> Embark on your journey of a lifetime with Traverse Nepal as your trusted
+                companion. Explore the untamed beauty of Nepal, immerse yourself in its rich history and culture, and
+                create memories that will last a lifetime. We are here to make your travel dreams come true, one step at
+                a time.
+                <br>
+                Experience Nepal like never before with Traverse Nepal - Your Ultimate Tourism Information System.
+            </p>
+            <div class="footer-icons">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-youtube"></i></a>
+            </div>
+        </div>
+    </div>
