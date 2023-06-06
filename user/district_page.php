8d6b34145e6969a2a8093cab16dfcaa688e11e48
pawanAXiX <pawanregmi86@gmail.com>
2023-06-06 23:53:11 +0545

    f

--- user/district_page.php
+++ user/district_page.php
@@ -1,10 +1,10 @@
 <?php
 
-session_start();
-if(!isset($_SESSION['email'])){
-    $_SESSION['Signed'] = "Please Login First";
-    header('location:login.php');
-}
+// session_start();
+// if(!isset($_SESSION['email'])){
+//     $_SESSION['Signed'] = "Please Login First";
+//     header('location:login.php');
+// }
  ?>
 
 <!DOCTYPE html>
