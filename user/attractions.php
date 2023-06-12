f69426dec048a43634d3f3eb1740d0980ae0d223
Basu Lamichhane <basulamichane991@gmail.com>
2023-06-12 17:29:52 +0545

    attraction page details created

---
+++ user/attractions.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Attractions List Page</title>
+    <link rel="stylesheet" href="style/header_style.css">
+    <link rel="stylesheet" href="style/footer_style.css">
+    <link rel="stylesheet" href="style/attractions_page_style.css">
+    <!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style/district_page_style.css"> -->
+    <!-- <script src="https://kit.fontawesome.com/ed931f650e.js" crossorigin="anonymous"></script> -->
+</head>
+
+<body>
+    <header>
+        <?php include "include/header.inc.php" ?>
+    </header>
+
+    <main class="attractions-list-container">
+        <div class="attractions-container">
+
+            <div class="attractions-list-title">
+                <div class="list-title">
+                    Attractions in
+                    <?php echo "kathmandu" ?>
+                </div>
+            </div>
+
+            <ul class="attractions-list-body">
+                <li class="attraction-content">
+                    <a href="">
+                        <div class="attraction-image-container">
+                            <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/60/d0/4b/boudhanath-stupa-kathmandu.jpg?w=1200&h=-1&s=1"
+                                alt="This is an image.">
+                        </div>
+
+                        <div class="bottom-content">
+                            <div class="attraction-name">
+                                1.Boudhanath Stupa
+                            </div>
+                            <div class="attraction-rating-review">
+                                <div class="attraction-rating">
+                                    <svg viewBox="0 0 128 24" width="68" height="12" aria-label="">
+                                        <path
+                                            d="M 12 0C5.388 0 0 5.388 0 12s5.388 12 12 12 12-5.38 12-12c0-6.612-5.38-12-12-12z"
+                                            transform=""></path>
+                                        <path
+                                            d="M 12 0C5.388 0 0 5.388 0 12s5.388 12 12 12 12-5.38 12-12c0-6.612-5.38-12-12-12z"
+                                            transform="translate(26 0)"></path>
+                                        <path
+                                            d="M 12 0C5.388 0 0 5.388 0 12s5.388 12 12 12 12-5.38 12-12c0-6.612-5.38-12-12-12z"
+                                            transform="translate(52 0)"></path>
+                                        <path
+                                            d="M 12 0C5.388 0 0 5.388 0 12s5.388 12 12 12 12-5.38 12-12c0-6.612-5.38-12-12-12z"
+                                            transform="translate(78 0)"></path>
+                                        <path
+                                            d="M 12 0C5.388 0 0 5.388 0 12s5.388 12 12 12 12-5.38 12-12c0-6.612-5.38-12-12-12zm0 2a9.983 9.983 0 019.995 10 10 10 0 01-10 10A10 10 0 012 12 10 10 0 0112 2z"
+                                            transform="translate(104 0)"></path>
+                                    </svg>
+                                </div>
+
+                                <div class="attraction-review">
+                                    5728
+                                </div>
+                            </div>
+                            <div class="attraction-type">
+                                Religious Temple
+                            </div>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+    </main>
+</body>
