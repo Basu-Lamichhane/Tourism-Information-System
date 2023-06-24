2d8904b40eeb87c22c868a76d701fbfc3764562b
Basu Lamichhane <basulamichane991@gmail.com>
2023-06-24 09:41:02 +0545

    updated search page

--- user/script/search.js
+++ user/script/search.js
@@ -1,6 +1,6 @@
-var district_option=document.getElementById("district");
-var destination_option=document.getElementById("destination");
-var type_option=document.getElementById("type");
+var district_option = document.getElementById("district");
+var destination_option = document.getElementById("destination");
+var type_option = document.getElementById("type");
 // for reset btn
 function resetBtn() {
     district_option.options[0].selected = true;
@@ -11,19 +11,20 @@ function resetBtn() {
 //for search focus-in and focus-out
 const SearchBar = document.getElementById('search');
 
-
 const suggestionDiv = document.getElementById('suggestions_container');
-
-var search_cancel_btn=document.getElementById("search-cancel-btn");
 
 
 SearchBar.addEventListener('focusin', function () {
-    suggestionDiv.style.display = (suggestionDiv.style.display === 'none') ? 'block' : 'none';
-    search_cancel_btn.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" height="20px" width="20px" version="1.1" id="Layer_1" viewBox="0 0 492 492" xml:space="preserve"><path d="M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872    c-5.068-5.076-11.824-7.856-19.036-7.856c-7.2,0-13.956,2.78-19.024,7.856L246.008,191.82L62.048,7.872    c-5.06-5.076-11.82-7.856-19.028-7.856c-7.2,0-13.96,2.78-19.02,7.856L7.872,23.988c-10.496,10.496-10.496,27.568,0,38.052    L191.828,246L7.872,429.952c-5.064,5.072-7.852,11.828-7.852,19.032c0,7.204,2.788,13.96,7.852,19.028l16.124,16.116    c5.06,5.072,11.824,7.856,19.02,7.856c7.208,0,13.968-2.784,19.028-7.856l183.96-183.952l183.952,183.952    c5.068,5.072,11.824,7.856,19.024,7.856h0.008c7.204,0,13.96-2.784,19.028-7.856l16.12-16.116    c5.06-5.064,7.852-11.824,7.852-19.028c0-7.204-2.792-13.96-7.852-19.028L300.188,246z"/></svg>';
+    // suggestionDiv.style.display = (suggestionDiv.style.display === 'none') ? 'block' : 'none';
+    suggestionDiv.style.display = 'block';
+
+    // adding the cancel icon if the search bar is focused
+    search_cancel_btn.innerHTML = '<button class="icon-wrap" id="cancel-btn"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="20px" width="20px" version="1.1" id="Layer_1" viewBox="0 0 492 492" xml:space="preserve"><path d="M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872    c-5.068-5.076-11.824-7.856-19.036-7.856c-7.2,0-13.956,2.78-19.024,7.856L246.008,191.82L62.048,7.872    c-5.06-5.076-11.82-7.856-19.028-7.856c-7.2,0-13.96,2.78-19.02,7.856L7.872,23.988c-10.496,10.496-10.496,27.568,0,38.052    L191.828,246L7.872,429.952c-5.064,5.072-7.852,11.828-7.852,19.032c0,7.204,2.788,13.96,7.852,19.028l16.124,16.116    c5.06,5.072,11.824,7.856,19.02,7.856c7.208,0,13.968-2.784,19.028-7.856l183.96-183.952l183.952,183.952    c5.068,5.072,11.824,7.856,19.024,7.856h0.008c7.204,0,13.96-2.784,19.028-7.856l16.12-16.116    c5.06-5.064,7.852-11.824,7.852-19.028c0-7.204-2.792-13.96-7.852-19.028L300.188,246z"/></svg></button>';
 });
-SearchBar.addEventListener('focusout', function () {
-    suggestionDiv.style.display = (suggestionDiv.style.display === 'none') ? 'block' : 'none';
-});
+// SearchBar.addEventListener('focusout', function () {
+//     suggestionDiv.style.display = 'none';
+//     search_cancel_btn.innerHTML='<button class="icon-wrap"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg></button>';
+// });
 
 
 function scrollContent(scrollAmount, action) {
@@ -37,3 +38,45 @@ function scrollContent(scrollAmount, action) {
         behavior: 'smooth'
     });
 }
+
+
+const searchBtn = document.getElementById("search-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+
+// searchBtn.addEventListener('click', function (event) {
+
+//     console.log("searchBtn clicked");
+//     event.preventDefault();
+//     suggestionDiv.style.display = 'block';
+//     search_cancel_btn.innerHTML = '<button class="icon-wrap" id="cancel-btn"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="20px" width="20px" version="1.1" id="Layer_1" viewBox="0 0 492 492" xml:space="preserve"><path d="M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872    c-5.068-5.076-11.824-7.856-19.036-7.856c-7.2,0-13.956,2.78-19.024,7.856L246.008,191.82L62.048,7.872    c-5.06-5.076-11.82-7.856-19.028-7.856c-7.2,0-13.96,2.78-19.02,7.856L7.872,23.988c-10.496,10.496-10.496,27.568,0,38.052    L191.828,246L7.872,429.952c-5.064,5.072-7.852,11.828-7.852,19.032c0,7.204,2.788,13.96,7.852,19.028l16.124,16.116    c5.06,5.072,11.824,7.856,19.02,7.856c7.208,0,13.968-2.784,19.028-7.856l183.96-183.952l183.952,183.952    c5.068,5.072,11.824,7.856,19.024,7.856h0.008c7.204,0,13.96-2.784,19.028-7.856l16.12-16.116    c5.06-5.064,7.852-11.824,7.852-19.028c0-7.204-2.792-13.96-7.852-19.028L300.188,246z"/></svg></button>';
+// });
+
+// cancelBtn.addEventListener('click', function (event) {
+
+//     console.log("CancelBtn clicked");
+//     event.preventDefault();
+//     suggestionDiv.style.display = 'none';
+//     search_cancel_btn.innerHTML = '<button class="icon-wrap" id="search-btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg></button>';
+// });
+
+
+
+var search_cancel_btn = document.getElementById("search-cancel-btn");
+
+search_cancel_btn.addEventListener('click', function (event) {
+
+    var searchOrCancelBtn=search_cancel_btn.firstElementChild;
+
+    if (searchOrCancelBtn.id === 'search-btn') {
+
+        event.preventDefault();
+        suggestionDiv.style.display = 'block';
+        search_cancel_btn.innerHTML = '<button class="icon-wrap" id="cancel-btn"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="20px" width="20px" version="1.1" id="Layer_1" viewBox="0 0 492 492" xml:space="preserve"><path d="M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872    c-5.068-5.076-11.824-7.856-19.036-7.856c-7.2,0-13.956,2.78-19.024,7.856L246.008,191.82L62.048,7.872    c-5.06-5.076-11.82-7.856-19.028-7.856c-7.2,0-13.96,2.78-19.02,7.856L7.872,23.988c-10.496,10.496-10.496,27.568,0,38.052    L191.828,246L7.872,429.952c-5.064,5.072-7.852,11.828-7.852,19.032c0,7.204,2.788,13.96,7.852,19.028l16.124,16.116    c5.06,5.072,11.824,7.856,19.02,7.856c7.208,0,13.968-2.784,19.028-7.856l183.96-183.952l183.952,183.952    c5.068,5.072,11.824,7.856,19.024,7.856h0.008c7.204,0,13.96-2.784,19.028-7.856l16.12-16.116    c5.06-5.064,7.852-11.824,7.852-19.028c0-7.204-2.792-13.96-7.852-19.028L300.188,246z"/></svg></button>';
+    } else if (searchOrCancelBtn.id === 'cancel-btn') {
+
+        event.preventDefault();
+        suggestionDiv.style.display = 'none';
+        search_cancel_btn.innerHTML = '<button class="icon-wrap" id="search-btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg></button>';
+        SearchBar.value="";
+    }
+});
--- user/search.php
+++ user/search.php
@@ -194,13 +194,15 @@ include "/include/star_rating.inc.php";
                         <div class="input-field">
                             <input id="search" type="text" placeholder="Type Keywords"
                                 onkeyup="javascript:(load_search_result(this.value))" />
-                            <button class="icon-wrap" id="search-cancel-btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                    <path
-                                        d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z">
-                                    </path>
-                                </svg>
-                            </button>
+                            <div id="search-cancel-btn">
+                                <button class="icon-wrap" id="search-btn">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                        <path
+                                            d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z">
+                                        </path>
+                                    </svg>
+                                </button>
+                            </div>
                         </div>
                     </div>
                     <div class="advance-search">
@@ -245,7 +247,7 @@ include "/include/star_rating.inc.php";
                         </div>
                         <div class="row">
                             <div class="input-field">
-                                <div class="result-count">
+                                <div class="result-count" id="count-result">
                                     <span id="no-of-results">0</span>results
                                 </div>
                                 <div class="group-btn">
@@ -265,7 +267,7 @@ include "/include/star_rating.inc.php";
         <div class="typeahead_suggestions_container" id="suggestions_container" style="display: none;">
 
             <div class="nearby_search">
-                <a href="#" target="_blank" class="suggestion_container" id="location-link";>
+                <a href="#" target="_blank" class="suggestion_container" id="location-link" ;>
                     <div class="suggestion-logo">
                         <svg viewBox="0 0 24 24" width="24px" height="24px">
                             <path fill-rule="evenodd" clip-rule="evenodd"
