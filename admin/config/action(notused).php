8d0bad1d96720a23be40d049f3bceb8a9cfde770
pawanAXiX <pawanregmi86@gmail.com>
2023-07-02 23:39:14 +0545

    dfsdf

--- admin/config/action(notused).php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_GET['type'])) {
-    if ($_GET['type'] == "attraction" || $_GET['type'] == "accommodation" || $_GET['type'] == "district" || $_GET['type'] == "cafe" || $_GET['type'] == "user") {
-        echo "<div  class='addPlace'><svg xmlns='http://www.w3.org/2000/svg' height='4em' viewBox='0 0 512 512'><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z'/></svg></div>";
-    }
-}
-?>
